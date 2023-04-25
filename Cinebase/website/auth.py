892cf9d3501ac8fda2db157f569af429253f14c1
Kartik Sharma <kartikdevasar@gmail.com>
2023-04-25 23:50:16 +0530

    Add files via upload

--- Cinebase/website/auth.py
+++ Cinebase/website/auth.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, g
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, logout_user, current_user
+# from flask_login import login_user, login_required, logout_user
 import psycopg2
 
+curr_user = "Guest"
 
 def get_db():
     if 'db' not in g:
@@ -23,7 +24,6 @@ def create_db_connection():
         user = "group_12",
         password = "ZzlQI7X4VqxdMJ" 
     )
-
     return conn
 
 auth = Blueprint('auth', __name__)
@@ -31,29 +31,35 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    global curr_user
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
 
-        # login_user(email, remember=True)
-        # user = User.query.filter_by(email=email).first()
-        # if user:
-        #     if check_password_hash(user.password, password):
-        #         flash('Logged in successfully!', category='success')
-        #         login_user(user, remember=True)
-        #         return redirect(url_for('views.home'))
-        #     else:
-        #         flash('Incorrect password, try again.', category='error')
-        # else:
-        #     flash('Email does not exist.', category='error')
+        conn = create_db_connection()
+        cur = conn.cursor()
 
-    return render_template("login.html", user=current_user)
+        cur.execute("SELECT * FROM users u where u.email = '{}'".format(email))
+        data = cur.fetchall()
+        
+        
+        if len(data) == 1:
+            if check_password_hash(data[0][1], password):
+                flash('Logged in successfully!', category='success')
+                curr_user = email
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html")
 
 
 @auth.route('/logout')
 # @login_required
 def logout():
-    # logout_user()
+    global curr_user
+    curr_user = "Guest"
     return redirect(url_for('auth.login'))
 
 
@@ -86,13 +92,14 @@ def sign_up():
             flash('Password must be at least 7 characters.', category='error')
         else:
             hsh = generate_password_hash(password1, method='sha256')
-            cmd = "INSERT INTO users (userid, password, username, email, birthdate) VALUES ({}, '{}', '{}', '{}', '{}');".format(1, hsh, first_name, email, dob)
+            cmd = "INSERT INTO users (userid, password, username, email, birthdate) VALUES ({}, '{}', '{}', '{}', '{}');".format(2, hsh, first_name, email, dob)
             cur.execute(cmd)
             conn.commit()
+            curr_user = email
             flash('Account created!', category='success')
 
         cur.close()
         conn.close()
         return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html", user=current_user)
+    return render_template("sign_up.html")
