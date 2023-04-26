92ab947616624a4a92c8356553ed57a5a96f42b8
AnaadKaur <99017703+AnaadKaur@users.noreply.github.com>
2023-04-26 19:17:49 +0530

    Create userdata.sql

---
+++ userdata.sql
@@ -0,0 +1,62 @@
+INSERT INTO users (emailid, username, password, location, birthdate)
+VALUES ('johndoe@gmail.com', 'JohnDoe', 'password123', 'Bristol', '1990-01-01');
+
+-- Inserting ratings for John Doe
+INSERT INTO rated (emailid, titleid, rating)
+VALUES ('johndoe@gmail.com', 'tt0127236', 8), ('johndoe@gmail.com', 'tt0183268', 9), ('johndoe@gmail.com', 'tt0111263', 10);
+----------------------------------------------------------------------------------------------------------
+
+INSERT INTO users (emailid, username, password, location, birthdate)
+VALUES ('sarahjones@gmail.com', 'sarahJ', 'NOpassword', 'New York', '2015-01-01');
+
+-- Inserting ratings for Sarah Jones
+INSERT INTO rated (emailid, titleid, rating)
+VALUES ('sarahjones@gmail.com', 'tt0116083', 7), ('sarahjones@gmail.com', 'tt0184926', 6);
+----------------------------------------------------------------------------------------------------------
+
+INSERT INTO users (emailid, username, password, location, birthdate)
+VALUES ('amy.johnson@hotmail.com', 'amyjohnson', 'passwordabc', 'Houston', '1998-03-25');
+
+-- Inserting ratings for Amy Johnson
+INSERT INTO rated (emailid, titleid, rating)
+VALUES ('amy.johnson@hotmail.com', 'tt0183268', 10), ('amy.johnson@hotmail.com', 'tt0116083', 8), ('amy.johnson@hotmail.com', 'tt0184926', 9);
+----------------------------------------------------------------------------------------------------------
+
+INSERT INTO users (emailid, username, password, location, birthdate)
+VALUES ('tombrown@hotmail.com', 'tommy', 'password456', 'Sydney, Australia', '1990-04-15');
+
+-- Inserting ratings for Tom Brown
+INSERT INTO rated (emailid, titleid, rating)
+VALUES ('tombrown@hotmail.com', 'tt0111263', 6), ('tombrown@hotmail.com', 'tt0116083', 7), ('tombrown@hotmail.com', 'tt0184926', 5);
+----------------------------------------------------------------------------------------------------------
+
+INSERT INTO users (emailid, username, password, location, birthdate)
+VALUES ('sara.kim@gmail.com', 'sarakhim', 'password!@#', 'Seoul, South Korea', '1995-02-28');
+
+-- Inserting ratings for Sara Kim
+INSERT INTO rated (emailid, titleid, rating)
+VALUES ('sara.kim@gmail.com', 'tt0127236', 9), ('sara.kim@gmail.com', 'tt0183268', 10), ('sara.kim@gmail.com', 'tt0116083', 8), ('sara.kim@gmail.com', 'tt0184926', 9);
+----------------------------------------------------------------------------------------------------------
+
+INSERT INTO users (emailid, username, password, location, birthdate)
+VALUES ('mohammed.ali@yahoo.com', 'mohammedali', 'passwordabc', 'Dubai, UAE', '1988-09-10');
+
+-- Inserting ratings for Mohammed Ali
+INSERT INTO rated (emailid, titleid, rating)
+VALUES ('mohammed.ali@yahoo.com', 'tt0127236', 7), ('mohammed.ali@yahoo.com', 'tt0116083', 6), ('mohammed.ali@yahoo.com', 'tt0184926', 8);
+----------------------------------------------------------------------------------------------------------
+
+INSERT INTO users (emailid, username, password, location, birthdate)
+VALUES('maria.santos@gmail.com', 'mariasantos', 'passworddef', 'Rio de Janeiro, Brazil', '1992-06-05');
+
+INSERT INTO users (emailid, username, password, location, birthdate)
+VALUES('thomas.wang@yahoo.com', 'thomaswang', 'passwordxyz', 'Shanghai, China', '1980-12-15');
+
+INSERT INTO users (emailid, username, password, location, birthdate)
+VALUES('javier.fernandez@hotmail.com', 'javierfernandez', 'password1234', 'Madrid, Spain', '1984-03-25');
+
+INSERT INTO users (emailid, username, password, location, birthdate)
+VALUES('olga.ivanova@gmail.com', 'olgai', 'password5678', 'Moscow, Russia', '1987-10-18');
+
+INSERT INTO users (emailid, username, password, location, birthdate)
+VALUES('chris.nguyen@yahoo.com', 'chrisnguyen', 'password999', 'Ho Chi Minh City, Vietnam', '1991-01-12');
