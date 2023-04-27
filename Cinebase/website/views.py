adb4e80fcef5dbd5b93272da56569204ca2383c1
Kartik Sharma <kartikdevasar@gmail.com>
2023-04-27 19:01:01 +0530

    Add files via upload

--- Cinebase/website/views.py
+++ Cinebase/website/views.py
@@ -18,6 +18,26 @@ def create_db_connection():
 
 views = Blueprint('views', __name__)
 
+bitval={'Documentary':0 ,  'Short':1 ,  'Animation':2, 'Comedy':3, 'Romance':4 ,'Sport':5, 'News':6,
+ 'Drama':7, 'Fantasy':8, 'Horror':9, 'Biography':10, 'Music':11, 'War':12, 'Crime':13, 'Western':14,
+ 'Family':15, 'Adventure':16, 'Action':17, 'History':18 ,'Mystery':19,  'Sci-Fi':20,
+ 'Musical':21, 'Thriller':22, 'Film-Noir':23, 'Talk-Show':24, 'Game-Show':25, 'Reality-TV':26,
+ 'Adult':27,'nan':28}
+
+def genres_to_list(x):
+    if x != None :
+        result = 0
+        for num in x:
+            if num in bitval:
+                # Use bitwise OR to set the corresponding bit to 1
+                result |= 1 << bitval[num]
+# Make sure the result is a 32-bit int
+        result &= 0xFFFFFFFF
+        return result
+    else :
+        result = 0
+        result &= 0xFFFFFFFF
+        return result
 
 
 @views.route('/', methods=['GET', 'POST'])
@@ -39,19 +59,61 @@ def home():
 @views.route('/movies', methods=['GET', 'POST'])
 def movies_stuff():
     if request.method == 'POST': 
-        movie = request.form.get('movie')#Gets the note from the HTML 
+        conn = create_db_connection()
+        cur = conn.cursor()
+        # cur.execute("SELECT * FROM users u where u.email = '{}'".format(email))
+        # data = cur.fetchall()
+
+        data = {}
+        for key, value in request.form.items():
+            if value == '':
+                data[key] = 'NULL'
+            elif not value.isdigit():
+                data[key] = '\'' + value + '\''
+            else:
+                data[key] = value
+
+        substring_tolookfor = data['subsrting_tolookfor']
+        num_votes_min = data['num_votes-min']
+        num_votes_max = data['num_votes-max']
+        release_date_min = data['release_date-min']
+        release_date_max = data['release_date-max']
+        runtime_min = data['runtime-min']
+        runtime_max = data['runtime-max']
+        moviemeter_min = data['moviemeter-min']
+        moviemeter_max = data['moviemeter-max']
+        genres = genres_to_list(request.form.getlist('genres'))
+
+        query = open('website/pysql/movie-master.txt', 'r').read()
+        formatted_query = query.format(
+            t_title=substring_tolookfor,
+            t_type='\'movie\'',
+            srt_yr=release_date_min,
+            end_yr=release_date_max,
+            is_adlt='NULL',
+            rn_time=runtime_max,
+            t_genre_list=genres,
+            cutOrIn='true'
+        )
+        cur.execute(formatted_query)
+        ret = cur.fetchall()
+
+        print(ret)
+        
+        # print(query)
+             
     return render_template("movies.html")
 
 @views.route('/people', methods=['GET', 'POST'])
 def people():
     if request.method == 'POST': 
-        person = request.form.get('p')#Gets the note from the HTML 
+        person = request.form.get('p') 
     return render_template("people.html")
 
 @views.route('/shows', methods=['GET', 'POST'])
 def shows():
     if request.method == 'POST': 
-        person = request.form.get('p')#Gets the note from the HTML 
+        person = request.form.get('p') 
     return render_template("shows.html")
 
 @views.route('/games', methods=['GET', 'POST'])
